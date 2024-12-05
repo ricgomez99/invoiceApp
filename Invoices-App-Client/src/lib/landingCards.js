dc4c8d227db4bc648def3376dc052040de4c7463
Andres <argomez.restrepo@gmail.com>
2024-12-05 13:09:35 -0500

    add new sections to landing page

---
+++ Invoices-App-Client/src/lib/landingCards.js
@@ -0,0 +1,20 @@
+export const landingCards = [
+  {
+    id: 1,
+    title: 'Streanlined Invoicing',
+    description:
+      'Effortlessly create professional invoices with customizable options. Add accurate data and maintain consistency across all your records.',
+  },
+  {
+    id: 2,
+    title: 'Track Purchases and Sales',
+    description:
+      'Stay organized with a clear overview of your transactions. Monitor the products you’ve bought or sold and manage them efficiently.',
+  },
+  {
+    id: 3,
+    title: 'Simplify Your Workflow',
+    description:
+      'Save time and reduce hassle with tools designed to integrate seamlessly into your daily operations. Focus on growing your business while we handle the details.',
+  },
+]
--- Invoices-App-Client/src/pages/Home/index.jsx
+++ Invoices-App-Client/src/pages/Home/index.jsx
@@ -2,6 +2,8 @@ import HomeHeader from '../../components/HomeHeader'
 import { Button } from '@material-tailwind/react'
 import { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
+import SimpleCard from '../../components/SimpleCard'
+import { landingCards } from '../../lib/landingCards'
 
 export default function Home() {
   const goTo = useNavigate()
@@ -11,12 +13,12 @@ export default function Home() {
     <section className="w-full flex flex-col items-center">
       <article className="flex flex-col w-full px-3 bg-[#eee] items-center">
         <HomeHeader />
-        <div className="w-full flex flex-row py-10 justify-between max-w-[1366px]">
+        <div className="w-full flex flex-col py-10 justify-between max-w-[1366px] lg:flex-row">
           <div className="flex flex-col text-left tracking-tight lg:max-w-xl">
             <h1 className="font-lato font-extrabold text-5xl text-gray-900 py-4">
               Creating invoices has never been easier!
             </h1>
-            <p className="font-lato text-[#616161] font-normal text-lg leading-8 my-6">
+            <p className="font-lato text-[#616161] font-normal text-lg leading-8 my-4 lg:my-6">
               Simplify your invoicing process with a platform designed to save
               you time and keep your business organized. Generate professional
               invoices effortlessly and take the hassle out of managing your
@@ -33,15 +35,37 @@ export default function Home() {
           <img
             src="public/illustration.png"
             alt="woman working on her invoices"
-            className="max-w-lg"
+            className="mt-6 max-w-full lg:mt-0 lg:max-w-lg"
           />
         </div>
       </article>
-      <article className="bg-white w-full flex flex-col px-3 items-center text-center py-10 max-w-[1366px]">
-        <h2 className="font-lato font-extrabold text-4xl text-gray-900 max-w-md">
+      <article className="bg-white w-full flex flex-col px-3 items-center text-center py-6 max-w-[1366px] lg:py-10">
+        <h2 className="font-lato font-extrabold text-4xl text-gray-900 max-w-md py-5">
           Manage them with confidence
         </h2>
-        <div></div>
+        <p className="font-lato font-normal text-base p-2 text-[#616161] max-w-2xl lg:p-5">
+          Generate invoices instantly and effortlessly. Take advantage of
+          versatile options to ensure accurate data entry. Keep a detailed
+          record of the products you have bought or sold, and seamlessly include
+          them in your invoices.
+        </p>
+        <img
+          src="public/innovate2.png"
+          alt="a man having success with his invoices"
+          className="max-w-full aspect-square object-cover rounded-xl pt-5 shadow-md lg:max-w-md"
+        />
+      </article>
+      <article className="flex flex-col px-3 py-6 w-full bg-[#eee] justify-center items-center lg:min-h-[680px] lg:py-0">
+        <div className="flex flex-col justify-center gap-6 self-center w-full max-w-6xl lg:flex-row">
+          {landingCards &&
+            landingCards.map((card) => (
+              <SimpleCard
+                key={card.id}
+                title={card.title}
+                description={card.description}
+              />
+            ))}
+        </div>
       </article>
     </section>
   )
