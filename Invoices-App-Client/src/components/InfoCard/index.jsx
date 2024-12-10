6b7a462cac03ddcf446c4ad1ab6f2965050c329c
Andres <argomez.restrepo@gmail.com>
2024-12-10 15:23:07 -0500

    improved landing page

---
+++ Invoices-App-Client/src/components/InfoCard/index.jsx
@@ -0,0 +1,24 @@
+export default function InfoCard({
+  description,
+  imageUrl,
+  direction,
+  altText,
+}) {
+  console.log(direction)
+  return (
+    <div
+      className={`flex p-3 max-w-lg justify-around items-center rounded-xl shadow-xl gap-5 ${
+        direction === 'left' ? 'flex-row' : 'flex-row-reverse'
+      }`}
+    >
+      <p className="font-lato font-normal text-xs text-[#616161]">
+        {description}
+      </p>
+      <img
+        src={imageUrl}
+        alt={altText}
+        className="rounded-full object-contain max-w-32 aspect-square"
+      />
+    </div>
+  )
+}
